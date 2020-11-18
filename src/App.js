d466b0b87478d0d9a7abc2c206dfd985f1b9f5d5
DuncanBScruggs <duncanbscruggs@gmail.com>
2020-11-18 13:23:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import NavBar from "./Components/NavBar";
+import Signup from "./Components/Signup";
+
 
 function App() {
+  const pages = [
+    { readableName: "Signup", url: "items" },
+  ]
+
+
+  const [currentPage, setCurrentPage] = useState(0)
+
+  // component did update
+  const setPage = (newPageNum) => {
+    setCurrentPage(newPageNum)
+    window.localStorage.setItem("currentPage", JSON.stringify(newPageNum))
+  }
+
+  let content = [<Signup />]
+
+
+  // did mount
+  useEffect(() => {
+    let lSpage = window.localStorage.getItem("currentPage")
+    if (lSpage !== currentPage) {
+      setCurrentPage(JSON.parse(lSpage))
+    }
+  }, [currentPage])
+
+  // useEffect(()=> window.localStorage.setItem("currentPage", JSON.stringify(currentPage)))
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="brand-main">
+      <NavBar
+        pages={pages}
+        currentPage={currentPage}
+        setPage={setPage}
+      />
+
+    <div className="container my-5">
+    {content[currentPage]}
+    </div>
+
     </div>
   );
 }
